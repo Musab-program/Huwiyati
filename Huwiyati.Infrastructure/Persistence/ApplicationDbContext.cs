542624f966e47baeddd93cc4b649475899fef154
musab-program <musabalnagri@gmail.com>
2026-09-06 18:16:41 +0300

    second push

--- Huwiyati.Infrastructure/Persistence/ApplicationDbContext.cs
+++ Huwiyati.Infrastructure/Persistence/ApplicationDbContext.cs
@@ -1,6 +1,27 @@
-﻿namespace Huwiyati.Infrastructure.Persistence;
+namespace Huwiyati.Infrastructure.Persistence;
 
-public class ApplicationDbContext
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Huwiyati.Infrastructure.Identity;
+using Huwiyati.Domain.Entities.CivilRegistry;
+using System.Reflection;
+
+public class ApplicationDbContext : IdentityDbContext<ApplicationUser, IdentityRole<Guid>, Guid>
 {
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
 
+    public DbSet<Person> Persons => Set<Person>();
+
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        // Automatically apply all configurations from current assembly (Infrastructure)
+        builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+    }
 }
